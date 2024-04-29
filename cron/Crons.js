75a007ba494dc4703db08b9a283e3c6b51540fd0
dhruvtechies <dhruvtechies0407@gmail.com>
2024-04-29 10:41:30 +0530

    localization

--- cron/Crons.js
+++ cron/Crons.js
@@ -3,9 +3,9 @@ const { clearOldOtps } = require("./task");
 
 
 // cron.schedule('* * * * *', () => { time durataion is 1minute  5 star ka matlb min hours month   year weak
-// cron.schedule('0 0 * * *'), () => {          //here the time duration is at 12 am at night means it will run at 12 oclock at night
+ cron.schedule('0 0 * * *', () => {          //here the time duration is at 12 am at night means it will run at 12 oclock at night
 
-cron.schedule('* * * * *', () => {
+//cron.schedule('* * * * *', () => {
     console.log('Checking and clearing old OTPs...');
     clearOldOtps();
 });
